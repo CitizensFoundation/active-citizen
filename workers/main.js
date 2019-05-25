0ccfd17a018a4b002f9e0dcdd8b1b403000d9dd3
Robert Bjarnason <robert.bjarnason@gmail.com>
2019-05-25 15:15:22 +0000

    Added locales

--- workers/main.js
+++ workers/main.js
@@ -20,7 +20,7 @@ var localesPath = path.resolve(__dirname, '../locales');
 i18n
   .use(Backend)
   .init({
-    preload: ['en', 'fr', 'it','da', 'kl', 'es', 'ca', 'hr', 'pt_BR', 'hu', 'tr', 'is', 'nl','no', 'pl', 'zh_TW'],
+    preload: ['en', 'fr', 'it','da', 'kl', 'es', 'sq','uz','uk', 'ca', 'hr', 'pt_BR', 'hu', 'tr', 'is', 'nl','no', 'pl', 'zh_TW'],
 
     fallbackLng:'en',
 
