a23e8a3b548d03eaacf2a5a59b6a3011cdbad4a3
Robert Bjarnason <robert.bjarnason@gmail.com>
2019-12-10 13:40:31 +0000

    Testing errors

--- workers/activity.js
+++ workers/activity.js
@@ -19,7 +19,7 @@ var ActivityWorker = function () {};
 
 ActivityWorker.prototype.process = function (activityJson, callback) {
   var activity;
-  log.info('Processing activity Started', { blah: fdsfsd.dsfdsfds.fsdfsd });
+  log.info('Processing activity Started');
   async.series([
     function (seriesCallback) {
       models.AcActivity.find({
