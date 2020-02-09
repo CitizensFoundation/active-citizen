ff8efe32679b43743729a7d53f7c896c36586b74
Robert Bjarnason <robert.bjarnason@gmail.com>
2020-02-09 16:56:13 +0000

    Work on analytics

--- engine/analytics/export_anon_community_activities.js
+++ engine/analytics/export_anon_community_activities.js
@@ -2,13 +2,16 @@ const models = require('../../../models');
 const _ = require('lodash');
 const async = require('async');
 const log = require('../../../utils/logger');
-const importDomain = require('./utils').importDomain;
-const importCommunity = require('./utils').importCommunity;
-const importGroup = require('./utils').importGroup;
-const importPost = require('./utils').importPost;
-const importPoint = require('./utils').importPoint;
 const crypto = require('crypto');
 const algorithm = 'aes-256-cbc';
+//const request = require('request');
+
+const request = {
+  post: (data, done) => {
+      console.log(JSON.stringify(data));
+      done();
+  }
+};
 
 const getEncryptedId = (id, key, iv) => {
   let cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(key), iv);
@@ -18,27 +21,107 @@ const getEncryptedId = (id, key, iv) => {
 };
 
 const saveAnonymousPost = (post, accessKey, done) => {
-  console.log(accessKey);
-  console.log(post);
-  done();
+  let expiryDate = new Date();
+  expiryDate.setDate(expiryDate.getDate() + 7);
+
+  const properties = {
+    id: post.id,
+    name: post.name,
+    date: post.created_at.toISOString(),
+    description: post.description,
+    description_en: post.description_en,
+    access_key: accessKey,
+    access_expires_at: expiryDate.toISOString()
+  };
+
+  const options = {
+    url: process.env["AC_ANALYTICS_BASE_URL"]+"anonPosts/"+process.env.AC_ANALYTICS_CLUSTER_ID+"/"+`${accessKey}${post.id}`,
+    headers: {
+      'X-API-KEY': process.env["AC_ANALYTICS_KEY"]
+    },
+    json: properties
+  };
+
+  request.post(options, (error) => {
+    done(error);
+  });
 };
 
 const saveAnonymousPoint = (point, accessKey, done) => {
-  console.log(accessKey);
-  console.log(point);
-  done();
+  let expiryDate = new Date();
+  expiryDate.setDate(expiryDate.getDate() + 7);
+
+  const properties = {
+    id: point.id,
+    date: point.created_at.toISOString(),
+    content: point.content,
+    content_en: point.content_en,
+    access_key: accessKey,
+    access_expires_at: expiryDate.toISOString()
+  };
+
+  const options = {
+    url: process.env["AC_ANALYTICS_BASE_URL"]+"anonPoints/"+process.env.AC_ANALYTICS_CLUSTER_ID+"/"+`${accessKey}${point.id}`,
+    headers: {
+      'X-API-KEY': process.env["AC_ANALYTICS_KEY"]
+    },
+    json: properties
+  };
+
+  request.post(options, (error) => {
+    done(error);
+  });
 };
 
 const saveActivity = (activity, accessKey, done) => {
-  console.log(accessKey);
-  console.log(activity);
-  done();
+  let expiryDate = new Date();
+  expiryDate.setDate(expiryDate.getDate() + 7);
+
+  const properties = {
+    id: activity.id,
+    date: activity.created_at.toISOString(),
+    type: activity.type,
+    point_id: activity.point_id,
+    post_id: activity.post_id,
+    user_id: activity.user_id,
+    access_key: accessKey,
+    access_expires_at: expiryDate.toISOString()
+  };
+
+  const options = {
+    url: process.env["AC_ANALYTICS_BASE_URL"]+"anonActivities/"+process.env.AC_ANALYTICS_CLUSTER_ID+"/"+`${accessKey}${activity.id}`,
+    headers: {
+      'X-API-KEY': process.env["AC_ANALYTICS_KEY"]
+    },
+    json: properties
+  };
+
+  request.post(options, (error) => {
+    done(error);
+  });
 };
 
-const saveAnonymousUser = (activity, accessKey, done) => {
-  console.log(accessKey);
-  console.log(activity);
-  done();
+const saveAnonymousUser = (user, accessKey, done) => {
+  let expiryDate = new Date();
+  expiryDate.setDate(expiryDate.getDate() + 7);
+
+  const properties = {
+    id: user.id,
+    access_key: accessKey,
+    access_expires_at: expiryDate.toISOString()
+  };
+
+  const options = {
+    url: process.env["AC_ANALYTICS_BASE_URL"]+"anonUsers/"+process.env.AC_ANALYTICS_CLUSTER_ID+"/"+`${accessKey}${user.id}`,
+    headers: {
+      'X-API-KEY': process.env["AC_ANALYTICS_KEY"]
+    },
+    json: properties
+  };
+
+  request.post(options, (error) => {
+    done(error);
+  });
 };
 
 const collectAllPostsAndPoints = (activities, collectedPostIds, collectedPointIds) => {
@@ -53,7 +136,7 @@ const collectAllPostsAndPoints = (activities, collectedPostIds, collectedPointId
   });
 };
 
-const sendAllCollectedPostAndPoints = (collectedPostIds, collectedPointIds, done) => {
+const sendAllCollectedPostAndPoints = (collectedPostIds, collectedPointIds, encryptionKey, encryptionIv, done) => {
   collectedPostIds = _.uniq(collectedPostIds);
   collectedPointIds = _.uniq(collectedPointIds);
 
@@ -81,9 +164,10 @@ const sendAllCollectedPostAndPoints = (collectedPostIds, collectedPointIds, done
             };
             models.AcTranslationCache.getTranslation(req, post, function (error, nameEn) {
               saveAnonymousPost({
-                id: getEncryptedId((post.id), randomContentEncryptionKey, randomContentEncryptionIv),
+                id: getEncryptedId((post.id), encryptionKey, encryptionIv),
                 name: post.name,
                 name_en: nameEn,
+                created_at: post.created_at,
                 description: post.description,
                 description_en: descriptionEn
               }, accessKey, forEachSeriesCallback);
@@ -105,7 +189,7 @@ const sendAllCollectedPostAndPoints = (collectedPostIds, collectedPointIds, done
           order: [
             [ models.PointRevision, 'created_at', 'asc' ],
           ],
-          attributes: ['id'],
+          attributes: ['id','created_at','content'],
           include: [
             {
               model: models.PointRevision,
@@ -121,8 +205,9 @@ const sendAllCollectedPostAndPoints = (collectedPostIds, collectedPointIds, done
           };
           models.AcTranslationCache.getTranslation(req, point, function (error, contentEn) {
             saveAnonymousPoint({
-              id: getEncryptedId((point.id), randomContentEncryptionKey, randomContentEncryptionIv),
+              id: getEncryptedId((point.id), encryptionKey, encryptionIv),
               content: point.content,
+              created_at: point.created_at,
               content_en: contentEn
             }, accessKey, forEachSeriesCallback);
 
@@ -149,7 +234,7 @@ const sendAllUserActivitiesWithContent = (userId, collectedPostIds, collectedPoi
           "activity.point.new","activity.point.helpful.new","activity.point.unhelpful.new"
         ]
       },
-      attributes: ['id','type'],
+      attributes: ['id','type','created_at','user_id'],
       include: [
         {
           model: models.Post,
@@ -169,8 +254,10 @@ const sendAllUserActivitiesWithContent = (userId, collectedPostIds, collectedPoi
       saveActivity({
         id: getEncryptedId(activity.id, encryptionKey, encryptionIv),
         type: activity.type,
+        created_at: activity.created_at,
         post_id: activity.Post ? getEncryptedId(activity.Post.id, encryptionKey, encryptionIv) : null,
-        point_id: activity.Point ? getEncryptedId(activity.Point.id, encryptionKey, encryptionIv) : null
+        point_id: activity.Point ? getEncryptedId(activity.Point.id, encryptionKey, encryptionIv) : null,
+        user_id: getEncryptedId(activity.user_id, encryptionKey, encryptionIv)
       }, accessKey, forEachSeriesCallback);
     }, (error) => {
       done(error);
@@ -218,36 +305,13 @@ const importCommunityUsersAndActivities = (communityId, accessKey, done) => {
     if (error) {
       done(error);
     } else {
-      sendAllCollectedPostAndPoints(collectedPostIds, collectedPointIds, (error) => {
+      sendAllCollectedPostAndPoints(collectedPostIds, collectedPointIds, encryptionKey, encryptionIv, (error) => {
         done(error);
       })
     }
   });
 };
 
-const updateCollection = (workPackage, done) => {
-  if (process.env.AC_ANALYTICS_KEY &&
-      process.env.AC_ANALYTICS_CLUSTER_ID &&
-      process.env.AC_ANALYTICS_BASE_URL) {
-    if (workPackage.domainId) {
-      updateDomain(workPackage.domainId, done)
-    } else if (workPackage.communityId) {
-      updateCommunity(workPackage.communityId, done)
-    } else if (workPackage.groupId) {
-      updateGroup(workPackage.groupId, done)
-    } else if (workPackage.postId) {
-      updatePost(workPackage.postId, done)
-    } else if (workPackage.pointId) {
-      updatePoint(workPackage.pointId, done)
-    } else {
-      done("Couldn't find any collection to update similarities", { workPackage });
-    }
-  } else {
-    log.warn("Can't find AC_ANALYTICS_KEY, AC_ANALYTICS_CLUSTER_ID & AC_ANALYTICS_BASE_URL for the similarities engine");
-    done();
-  }
-};
-
 module.exports = {
-  updateCollection
+  importCommunityUsersAndActivities
 };
