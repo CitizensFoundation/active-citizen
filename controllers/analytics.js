914eac8f003a92e004390668e6a2c927736c8007
Robert Bjarnason <robert.bjarnason@gmail.com>
2020-02-22 22:55:52 +0000

    Work on analytics

--- controllers/analytics.js
+++ controllers/analytics.js
@@ -6,6 +6,7 @@ const log = require('../utils/logger');
 const toJson = require('../utils/to_json');
 const moment = require('moment');
 const _ = require('lodash');
+const request = require('request');
 
 const getPointDomainIncludes = (id) => {
   return [
@@ -95,11 +96,16 @@ const getGroupIncludes = (id) => {
 };
 
 const countModelRowsByTimePeriod = (model, whereOptions, includeOptions, done) => {
+  //TODO: Add 5 minute caching
   model.findAll({
     where: whereOptions,
     include: includeOptions,
-    attributes: ['created_at']
+    attributes: ['created_at'],
+    order: ['created_at','ASC']
   }).then((results) => {
+    let startDate = moment(results[0].created_at);
+    const endDate = moment(results[results.length-1].created_at);
+
     const days = _.groupBy(results, function (item) {
       return moment(item.created_at).format("DD/MM/YYYY");
     });
@@ -124,6 +130,27 @@ const countModelRowsByTimePeriod = (model, whereOptions, includeOptions, done) =
       return { date: moment(items[0].created_at).format("YYYY"), count: items.length }
     });
 
+    const totalDaysCount = endDate.diff(startDate, 'd', false);
+    for (let i = 1; i < totalDaysCount; i++) {
+      dayCounts.splice(i,0, {"date" : startDate.add(1, 'd').toISOString(), count: 0})
+    }
+
+    startDate = moment(results[0].created_at);
+
+    const totalMonthsCount = endDate.diff(startDate, 'm', false);
+
+    for (let i = 1; i < totalMonthsCount; i++) {
+      months.splice(i,0, {"date" : startDate.add(1, 'm').toISOString(), count: 0})
+    }
+
+    startDate = moment(results[0].created_at);
+
+    const totalYearsCount = endDate.diff(startDate, 'y', false);
+
+    for (let i = 1; i < totalYearsCount; i++) {
+      months.splice(i,0, {"date" : startDate.add(1, 'y').toISOString(), count: 0})
+    }
+
     dayCounts = _.orderBy(dayCounts, ['date'], ['asc']);
     monthsCounts = _.orderBy(monthsCounts, ['date'], ['asc']);
     yearsCounts = _.orderBy(yearsCounts, ['date'], ['asc']);
@@ -138,36 +165,6 @@ const countModelRowsByTimePeriod = (model, whereOptions, includeOptions, done) =
 countModelRowsByTimePeriod(models.AcActivity, {
   type: {
     $in: [
-      "activity.post.new","activity.post.opposition.new","activity.post.endorsement.new",
-      "activity.point.new","activity.point.helpful.new","activity.point.unhelpful.new"
-    ]
-  }
-}, getCommunityIncludes(1017), 'day', (results) => {
-  var a = results;
-});
-
-countModelRowsByTimePeriod(models.Point, {}, getPointDomainIncludes(1), (error, results) => {
-  var a = results;
-});
-
-countModelRowsByTimePeriod(models.Post, {}, getDomainIncludes(1), (error, results) => {
-  var a = results;
-});
-
-countModelRowsByTimePeriod(models.AcActivity, {
-  type: {
-    $in: [
-      "activity.post.opposition.new","activity.post.endorsement.new",
-      "activity.point.helpful.new","activity.point.unhelpful.new"
-    ]
-  }
-}, getGroupIncludes(47), 'day', (results) => {
-  var a = results;
-});
-
-countModelRowsByTimePeriod(models.AcActivity, {
-  type: {
-    $in: [
       "activity.user.login"
     ]
   },
@@ -179,101 +176,164 @@ countModelRowsByTimePeriod(models.AcActivity, {
 */
 
 const getFromAnalyticsApi = (featureType, collectionType, collectionId, done) => {
+  //TODO: Implement cache
   const options = {
-    url: process.env["AC_ANALYTICS_BASE_URL"]+featureType+"/"+process.env.AC_ANALYTICS_CLUSTER_ID+"/"+collectionId,
+    url: process.env["AC_ANALYTICS_BASE_URL"]+featureType+"/"+collectionType+"/"+process.env.AC_ANALYTICS_CLUSTER_ID+"/"+collectionId,
     headers: {
       'X-API-KEY': process.env["AC_ANALYTICS_KEY"]
     }
   };
 
   request.get(options, (error, content) => {
+    if (content && content.statusCode!=200) {
+      error = content.statusCode;
+    }
     done(error, content);
   });
 };
 
-router.get('/communities/:id/wordcloud', /*auth.can('edit community'),*/ function(req, res) {
-  getFromAnalyticsApi("wordcloud", "community", req.params.id, function (error, content) {
-    if (error) {
-      log.error("Analytics Wordcloud Error Community", { communityId: req.params.id, userId: req.user ? req.user.id : null, errorStatus:  500 });
-      res.sendStatus(500);
-    } else {
-      res.send(content);
+const triggerSimilaritiesTraining = (collectionType, collectionId, done) => {
+  const options = {
+    url: process.env["AC_ANALYTICS_BASE_URL"]+"trigger_similarities_training"+"/"+collectionType+"/"+process.env.AC_ANALYTICS_CLUSTER_ID+"/"+collectionId,
+    headers: {
+      'X-API-KEY': process.env["AC_ANALYTICS_KEY"]
+    },
+    json: {}
+  };
+
+  request.put(options, (error, content) => {
+    if (content && content.statusCode!=200) {
+      error = content.statusCode;
     }
+    done(error, content);
+  });
+};
+
+const sendBackResultsOrError = (req,res,type,error,results) => {
+  if (error) {
+    log.error("Analytics Error", { id: req.params.id, url: req.url, userId: req.user ? req.user.id : null, errorStatus:  500 });
+    res.sendStatus(500);
+  } else {
+    res.send(content);
+  }
+};
+
+// WORD CLOUD
+router.get('/domains/:id/wordcloud', /*auth.can('edit community'),*/ function(req, res) {
+  getFromAnalyticsApi("wordclouds", "domain", req.params.id, function (error, content) {
+    sendBackResultsOrError(req,res,error,content);
+  });
+});
+
+router.get('/communities/:id/wordcloud', /*auth.can('edit community'),*/ function(req, res) {
+  getFromAnalyticsApi("wordclouds", "community", req.params.id, function (error, content) {
+    sendBackResultsOrError(req,res,error,content);
+  });
+});
+
+router.get('/groups/:id/wordcloud', /*auth.can('edit community'),*/ function(req, res) {
+  getFromAnalyticsApi("wordclouds", "group", req.params.id, function (error, content) {
+    sendBackResultsOrError(req,res,error,content);
+  });
+});
+
+// SIMILARITIES
+router.get('/domains/:id/similarities_weights', /*auth.can('edit community'),*/ function(req, res) {
+  getFromAnalyticsApi("similarities_weights", "domain", req.params.id, function (error, content) {
+    sendBackResultsOrError(req,res,error ? error : content.body ? null : 'noBody', JSON.parse(content.body));
   });
 });
 
 router.get('/communities/:id/similarities_weights', /*auth.can('edit community'),*/ function(req, res) {
   getFromAnalyticsApi("similarities_weights", "community", req.params.id, function (error, content) {
-    if (error) {
-      log.error("Analytics Wordcloud Error Community", { communityId: req.params.id, userId: req.user ? req.user.id : null, errorStatus:  500 });
-      res.sendStatus(500);
-    } else {
-      res.send(content);
-    }
+    sendBackResultsOrError(req,res,error ? error : content.body ? null : 'noBody', JSON.parse(content.body));
   });
 });
 
-router.get('/communities/:id/similarities_weights', auth.can('edit community'), function(req, res) {
-  const options = {
-    community_id: req.params.id,
-    noBulkOperations: true
-  };
-  getActivities(req, res, options, function (error) {
-    if (error) {
-      log.error("Activities Error Community", { communityId: req.params.id, userId: req.user ? req.user.id : null, errorStatus:  500 });
-      res.sendStatus(500);
-    }
+router.get('/groups/:id/similarities_weights', /*auth.can('edit community'),*/ function(req, res) {
+  getFromAnalyticsApi("similarities_weights", "group", req.params.id, function (error, content) {
+    sendBackResultsOrError(req,res,error ? error : content.body ? null : 'noBody', JSON.parse(content.body));
   });
 });
 
-router.get('/domains/:id', auth.can('view domain'), function(req, res) {
-  const options = {
-    domain_id: req.params.id,
-    noBulkOperations: true
-  };
-  getActivities(req, res, options, function (error) {
-    if (error) {
-      log.error("Activities Error Domain", { domainId: req.params.id, userId: req.user ? req.user.id : null, errorStatus:  500 });
-      res.sendStatus(500);
-    }
+// STATS
+router.get('/domains/:id/stats_posts', /*auth.can('edit community'),*/ function(req, res) {
+  countModelRowsByTimePeriod(models.Post, {}, getPostDomainIncludes(req.params.id), (error, results) => {
+    sendBackResultsOrError(req,res,error, results);
   });
 });
 
-router.get('/communities/:id', auth.can('view community'), function(req, res) {
-  const options = {
-    community_id: req.params.id,
-    noBulkOperations: true
-  };
-  getActivities(req, res, options, function (error) {
-    if (error) {
-      log.error("Activities Error Community", { communityId: req.params.id, userId: req.user ? req.user.id : null, errorStatus:  500 });
-      res.sendStatus(500);
-    }
+router.get('/domains/:id/stats_points', /*auth.can('edit community'),*/ function(req, res) {
+  countModelRowsByTimePeriod(models.Point, {}, getPointDomainIncludes(req.params.id), (error, results) => {
+    sendBackResultsOrError(req,res,error, results);
   });
 });
 
-router.get('/groups/:id', auth.can('view group'), function(req, res) {
-  const options = {
-    group_id: req.params.id
-  };
-  getActivities(req, res, options, function (error) {
-    if (error) {
-      log.error("Activities Error Group", { groupId: req.params.id, userId: req.user ? req.user.id : null, errorStatus:  500 });
-      res.sendStatus(500);
+router.get('/domains/:id/stats_votes', /*auth.can('edit community'),*/ function(req, res) {
+  countModelRowsByTimePeriod(models.AcActivity, {
+    type: {
+      $in: [
+        "activity.post.opposition.new","activity.post.endorsement.new",
+        "activity.point.helpful.new","activity.point.unhelpful.new"
+      ]
     }
+  }, getDomainIncludes(req.params.id), 'day', (error, results) => {
+    sendBackResultsOrError(req,res,error,results);
   });
 });
 
-router.get('/posts/:id', auth.can('view post'), function(req, res) {
-  const options = {
-    post_id: req.params.id
-  };
-  getActivities(req, res, options, function (error) {
-    if (error) {
-      log.error("Activities Error Group", { postId: req.params.id, userId: req.user ? req.user.id : null, errorStatus:  500 });
-      res.sendStatus(500);
-    }
+router.get('/communities/:id/stats_posts', /*auth.can('edit community'),*/ function(req, res) {
+  countModelRowsByTimePeriod(models.Post, {}, getPostCommunityIncludes(req.params.id), (error, results) => {
+    sendBackResultsOrError(req,res,error, results);
   });
 });
+
+router.get('/communities/:id/stats_points', /*auth.can('edit community'),*/ function(req, res) {
+  countModelRowsByTimePeriod(models.Point, {}, getPointCommunityIncludes(req.params.id), (error, results) => {
+    sendBackResultsOrError(req,res,error, results);
+  });
+});
+
+router.get('/communities/:id/stats_votes', /*auth.can('edit community'),*/ function(req, res) {
+  countModelRowsByTimePeriod(models.AcActivity, {
+    type: {
+      $in: [
+        "activity.post.opposition.new","activity.post.endorsement.new",
+        "activity.point.helpful.new","activity.point.unhelpful.new"
+      ]
+    }
+  }, getCommunityIncludes(req.params.id), 'day', (error, results) => {
+    sendBackResultsOrError(req,res,error,results);
+  });
+});
+
+router.get('/groups/:id/stats_posts', /*auth.can('edit community'),*/ function(req, res) {
+  countModelRowsByTimePeriod(models.Post, {}, getPostGroupIncludes(req.params.id), (error, results) => {
+    sendBackResultsOrError(req,res,error, results);
+  });
+});
+
+router.get('/groups/:id/stats_points', /*auth.can('edit community'),*/ function(req, res) {
+  countModelRowsByTimePeriod(models.Point, {}, getPointGroupIncludes(req.params.id), (error, results) => {
+    sendBackResultsOrError(req,res,error, results);
+  });
+});
+
+router.get('/groups/:id/stats_votes', /*auth.can('edit community'),*/ function(req, res) {
+  countModelRowsByTimePeriod(models.AcActivity, {
+    type: {
+      $in: [
+        "activity.post.opposition.new","activity.post.endorsement.new",
+        "activity.point.helpful.new","activity.point.unhelpful.new"
+      ]
+    }
+  }, getGroupIncludes(req.params.id), 'day', (error, results) => {
+    sendBackResultsOrError(req,res,error,results);
+  });
+});
+
+
+
+
 
 module.exports = router;
