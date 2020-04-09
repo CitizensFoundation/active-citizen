5f2ac10a2642cad374493668c8a306617db97899
Robert Bjarnason <robert.bjarnason@gmail.com>
2020-04-09 22:56:48 +0000

    Work on reports

---
+++ workers/reports.js
@@ -0,0 +1,30 @@
+const async = require("async");
+const models = require("../../models");
+const log = require('../utils/logger');
+const queue = require('./queue');
+const i18n = require('../utils/i18n');
+const toJson = require('../utils/to_json');
+const _ = require('lodash');
+const fs = require('fs');
+
+let airbrake = null;
+if(process.env.AIRBRAKE_PROJECT_ID) {
+  airbrake = require('../utils/airbrake');
+}
+
+let ReportsWorker = function () {};
+
+ReportsWorker.prototype.process = (workPackage, callback) => {
+  switch (workPackage.type) {
+    case 'start-docx-report-generation':
+      startDocxReportGeneration(workPackage, callback);
+      break;
+    case 'start-xls-report-generation':
+      startXlsReportGeneration(workPackage, callback);
+      break;
+    default:
+      callback("Unknown type for workPackage: " + workPackage.type);
+  }
+};
+
+module.exports = new ReportsWorker();
