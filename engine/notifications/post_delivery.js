de2bf7405a1a8d4be70d55e956e84a9e048a96ea
Robert Bjarnason <robert.bjarnason@gmail.com>
2020-05-04 19:34:53 +0000

    Fix

--- engine/notifications/post_delivery.js
+++ engine/notifications/post_delivery.js
@@ -7,7 +7,9 @@ module.exports = function (notification, user, callback) {
   var post = notification.AcActivities[0].Post;
   var postName = post ? post.name : "?";
 
-  if (notification.type=='notification.post.new') {
+  if (!post) {
+    callback("No public post found for notification delivery");
+  } else if (notification.type=='notification.post.new') {
     filterNotificationForDelivery(notification, user, 'post_activity', { translateToken: 'notification.email.postNew', contentName: postName }, callback);
   } else if (notification.type=='notification.post.endorsement') {
     var subjectTranslateToken = (notification.AcActivities[0].type.indexOf('activity.post.endorsement') > -1) ? 'notification.email.postEndorsement' : 'notification.email.postOpposition';
