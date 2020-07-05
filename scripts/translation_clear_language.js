d552f810c6e9b23d66fa77baa1b188b7a02b2cb3
Robert Bjarnason <robert.bjarnason@gmail.com>
2020-07-05 18:57:40 +0000

    Clear language

--- scripts/translation_clear_language.js
+++ scripts/translation_clear_language.js
@@ -29,6 +29,81 @@ if (objectType==="point") {
     console.error(error);
     process.exit();
   });
+} else if (objectType==="group") {
+  models.Group.findOne({
+    where: {
+      id: objectId
+    },
+    attributes: ['id','language']
+  }).then((result) => {
+    if (result) {
+      console.log(`Clearing current group language of ${result.language}`);
+      result.set('language', null);
+      result.save().then(function () {
+        console.log(`Cleared language for group ${result.id}`);
+        process.exit();
+      }).catch((error)=>{
+        console.error(error);
+        process.exit();
+      });
+    } else {
+      console.warn("Not found: "+indexKey);
+      process.exit();
+    }
+  }).catch((error)=>{
+    console.error(error);
+    process.exit();
+  });
+} else if (objectType==="community") {
+  models.Community.findOne({
+    where: {
+      id: objectId
+    },
+    attributes: ['id','language']
+  }).then((result) => {
+    if (result) {
+      console.log(`Clearing current community language of ${result.language}`);
+      result.set('language', null);
+      result.save().then(function () {
+        console.log(`Cleared language for community ${result.id}`);
+        process.exit();
+      }).catch((error)=>{
+        console.error(error);
+        process.exit();
+      });
+    } else {
+      console.warn("Not found: "+indexKey);
+      process.exit();
+    }
+  }).catch((error)=>{
+    console.error(error);
+    process.exit();
+  });
+} else if (objectType==="domain") {
+  models.Domain.findOne({
+    where: {
+      id: objectId
+    },
+    attributes: ['id','language']
+  }).then((result) => {
+    if (result) {
+      console.log(`Clearing current domain language of ${result.language}`);
+      result.set('language', null);
+      result.save().then(function () {
+        console.log(`Cleared language for domain ${result.id}`);
+        process.exit();
+      }).catch((error)=>{
+        console.error(error);
+        process.exit();
+      });
+    } else {
+      console.warn("Not found: "+indexKey);
+      process.exit();
+    }
+  }).catch((error)=>{
+    console.error(error);
+    process.exit();
+  });
 } else {
   console.log("No object type selected");
   process.exit();
