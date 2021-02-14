50f23434f8f2a8a26a3ce0282de03c1755320c28
Robert Bjarnason <robert.bjarnason@gmail.com>
2021-02-14 19:57:32 +0000

    Fixes for cloning

--- utils/translation_cloning.js
+++ utils/translation_cloning.js
@@ -5,7 +5,7 @@ const _ = require('lodash');
 const fs = require('fs');
 const request = require('request');
 const farmhash = require('farmhash');
-const fixTargetLocale = require('translation_helpers').fixTargetLocale;
+const fixTargetLocale = require('./translation_helpers').fixTargetLocale;
 
 // For post get translations in all locales (that exists)
 // select * from translation_cache where index_key LIKE 'communityContent-728-%-3536451109';
@@ -16,326 +16,111 @@ const fixTargetLocale = require('translation_helpers').fixTargetLocale;
 // Groups
 // Points
 
-
-const addItem = (targetLocale, items, textType, id, content, done) => {
-  if (!content) {
-    done();
-  } else {
-    const indexKey = `${textType}-${id}-${fixTargetLocale(targetLocale)}-${farmhash.hash32(content).toString()}`;
-    models.AcTranslationCache.findOne({
-      where: {
-        index_key: indexKey
-      }
-    }).then((result) => {
-      const item = {};
-      item.textType = textType;
-      item.contentId = id;
-      item.originalText = content;
-      item.indexKey = indexKey;
-      if (result) {
-        item.translatedText = result.content;
-      }
-      items.push(item);
-      done();
-    }).catch((error)=>{
-      done(error);
-    });
-  }
-};
-
-const addTranslationsForPosts = (targetLocale, items, posts, done) => {
-  async.forEachSeries(posts, (post, forEachCallback) => {
-    addItem(targetLocale, items, 'postName', post.id, post.name, (error) => {
-      if (error) {
-        forEachCallback(error)
-      } else {
-        addItem(targetLocale, items,'postContent', post.id, post.description, (error) => {
-          forEachCallback(error);
-        });
-      }
-    });
-  }, error => {
-    done(error);
-  })
-};
-
-const addTranslationsForGroups = (targetLocale, items, groups, done) => {
-  async.forEachSeries(groups, (group, forEachCallback) => {
-    addItem(targetLocale, items, 'groupName', group.id, group.name, (error) => {
-      if (error) {
-        forEachCallback(error)
-      } else {
-        addItem(targetLocale, items, 'groupContent', group.id, group.objectives, (error) => {
-          forEachCallback(error);
-        });
-      }
-    });
-  }, error => {
-    done(error);
-  })
-};
-
-const addTranslationsForCommunity = (targetLocale, items, community, done) => {
-  addItem(targetLocale, items, 'communityName', community.id, community.name, (error) => {
-    if (error) {
-      done(error)
-    } else {
-      addItem(targetLocale, items, 'communityContent', community.id, community.description, (error) => {
-        done(error);
-      });
-    }
-  });
-};
-
-const getTranslatedTextsForCommunity = (targetLocale, communityId, done) => {
-  const communityItems = [];
-  const groupItems = [];
-  const postItems = [];
-
-  async.parallel([
-    (seriesCallback) => {
-      async.series([
-        (innerSeriesCallback) => {
-          models.Post.findAll({
-            attributes: ['id','name','description'],
-            include: [
-              {
-                model: models.Group,
-                attributes: ['id'],
-                include: [
-                  {
-                    model: models.Community,
-                    attributes: ['id'],
-                    where: {
-                      id: communityId
-                    }
-                  }
-                ]
-              }
-            ]
-          }).then(posts=>{
-            addTranslationsForPosts(targetLocale, postItems, posts, innerSeriesCallback);
-          }).catch(error=>{
-            innerSeriesCallback(error);
-          })
-        },
-        (innerSeriesCallback) => {
-          models.Group.findAll({
-            attributes: ['id','name','objectives'],
-            where: {
-              community_id: communityId
-            }
-          }).then(groups=>{
-            addTranslationsForGroups(targetLocale, groupItems, groups, innerSeriesCallback);
-          }).catch(error=>{
-            innerSeriesCallback(error);
-          })
-        },
-        (innerSeriesCallback) => {
-          models.Community.findOne({
-            attributes: ['id','name','description'],
-            where: {
-              id: communityId
-            }
-          }).then(community=>{
-            addTranslationsForCommunity(targetLocale, communityItems, community, innerSeriesCallback);
-          }).catch(error=>{
-            innerSeriesCallback(error);
-          })
-        }
-      ], (error) => {
-        seriesCallback(error);
-      })
-    },
-  ], error => {
-    if (error) {
-      done(null, error);
-    } else {
-      done(communityItems.concat(groupItems.concat(postItems)));
-    }
-  });
-}
-
-const getTranslatedTextsForGroup = (targetLocale, groupId, done) => {
-  const groupItems = [];
-  const postItems = [];
-
-  async.parallel([
-    (seriesCallback) => {
-      async.series([
-        (innerSeriesCallback) => {
-          models.Post.findAll({
-            attributes: ['id','name','description'],
-            include: [
-              {
-                model: models.Group,
-                attributes: ['id'],
-                where: {
-                  id: groupId
-                }
-              }
-            ]
-          }).then(posts=>{
-            addTranslationsForPosts(targetLocale, postItems, posts, innerSeriesCallback);
-          }).catch(error=>{
-            innerSeriesCallback(error);
-          })
-        },
-        (innerSeriesCallback) => {
-          models.Group.findAll({
-            attributes: ['id','name','objectives'],
-            where: {
-              id: groupId
-            }
-          }).then(groups=>{
-            addTranslationsForGroups(targetLocale, groupItems, groups, innerSeriesCallback);
-          }).catch(error=>{
-            innerSeriesCallback(error);
-          })
-        }
-      ], (error) => {
-        seriesCallback(error);
-      })
-    },
-  ], error => {
-    if (error) {
-      done(null, error);
-    } else {
-      done(groupItems.concat(postItems));
-    }
-  });
-}
-
-const updateTranslation = (item, done) => {
-  let targetLocale = fixTargetLocale(item.targetLocale);
-
-  const indexKey = `${item.textType}-${item.contentId}-${targetLocale}-${farmhash.hash32(item.content).toString()}`;
-  models.AcTranslationCache.findOrCreate({
+const getTranslationsForSearch = (textType, id, content, callback) => {
+  const indexSearch = `${textType}-${id}-%-${farmhash.hash32(content).toString()}`;
+  models.AcTranslationCache.findAll({
     where: {
-      index_key: indexKey
-    },
-    defaults: {
-      index_key: indexKey,
-      content: item.translatedText
+      index_key: {
+        $like: indexSearch
+      }
     }
-  }).then((result) => {
-    if (result && result.length>0) {
-      result[0].content = item.translatedText;
-      result[0].save().then(()=>{
-        done();
-      }).catch(error => seriesCallback(error));
-    } else {
-      seriesCallback("Cant find or create translation");
-    }
-  }).catch(error => done(error));
+  }).then( translations => {
+    callback(null, translations);
+  }).catch( error=> {
+    callback(error);
+  })
 }
 
-const updateTranslationForGroup = (groupId, item, done) => {
-  async.series([
-    (seriesCallback) => {
-      if (["groupName","groupContent"].indexOf(item.textType)>-1) {
-        seriesCallback(groupId==item.contentId ? null : 'Access denied')
-      } else {
-        seriesCallback();
-      }
-    },
-    (seriesCallback) => {
-      if (["postName","postContent"].indexOf(item.textType)>-1) {
-        models.Post.findOne({
-          where: {
-            id: item.contentId
-          },
-          attributes: ["id"],
-          include: [
-            {
-              model: models.Group,
-              required: true,
-              where: { id: groupId }
-            }
-          ]
-        }).then( post => {
-          seriesCallback( post ? null : 'Access denied')
-        }).catch(error => seriesCallback(error));
-      } else {
-        seriesCallback();
-      }
-    },
-    (seriesCallback) => {
-      updateTranslation(item, seriesCallback);
-    },
-  ], error => {
-    done(error);
-  })
-};
+const updateIndexKey = (current_index_key, objectId) => {
+  const splitIndex = current_index_key.split("-");
+  splitIndex[1] = objectId;
+  const newIndexKey = splitIndex.join("-");
+  return newIndexKey;
+}
 
-const updateTranslationForCommunity = (communityId, item, done) => {
-  async.series([
-    (seriesCallback) => {
-      if (["communityName","communityContent"].indexOf(item.textType)>-1) {
-        seriesCallback(communityId==item.contentId ? null : 'Access denied')
-      } else {
-        seriesCallback();
+const cloneTranslations = (translations, objectId, callback) => {
+  async.forEach(translations, (translation, forEachCallback) => {
+    const newTranslationJson = JSON.parse(JSON.stringify(translation.toJSON()));
+    delete newTranslationJson['id'];
+    newTranslationJson['index_key'] = updateIndexKey(translation.index_key, objectId);
+    const newTranslation = models.AcTranslationCache.build(newTranslationJson);
+    newTranslation.save().then(()=>{
+      forEachCallback();
+    }).catch( error=> {
+      callback(error);
+    });
+  }, (error) => {
+    callback(error);
+  })
+}
+
+const cloneTranslationForItem = (textType, inObjectId, outObjectId, content, callback) => {
+  getTranslationsForSearch(textType, inObjectId, content, (error, translations) => {
+    if (error) {
+      callback(error);
+    } else {
+      if (translations.length>0) {
+        cloneTranslations(translations, outObjectId, callback);
+      }  else {
+        callback();
       }
+    }
+  })
+}
+
+const cloneTranslationForPost = (inPost, outPost, done) => {
+  async.parallel([
+    (parallelCallback) => {
+      cloneTranslationForItem("postName", inPost.id, outPost.id, inPost.name, parallelCallback);
     },
-    (seriesCallback) => {
-      if (["groupName","groupContent"].indexOf(item.textType)>-1) {
-        models.Group.findOne({
-          where: {
-            id: item.contentId
-          },
-          attributes: ["id"],
-          include: [
-            {
-              model: models.Community,
-              where: { id: communityId },
-              required: true
-            }
-          ]
-        }).then( group => {
-          seriesCallback( group ? null : 'Access denied')
-        }).catch(error => seriesCallback(error));
-      } else {
-        seriesCallback();
-      }
-    },
-    (seriesCallback) => {
-      if (["postName","postContent"].indexOf(item.textType)>-1) {
-        models.Post.findOne({
-          where: {
-            id: item.contentId
-          },
-          attributes: ["id"],
-          include: [
-            {
-              model: models.Group,
-              required: true,
-              include: [
-                {
-                  model: models.Community,
-                  where: { id: communityId },
-                  required: true
-                }
-              ]
-            }
-          ]
-        }).then( post => {
-          seriesCallback( post ? null : 'Access denied')
-        }).catch(error => seriesCallback(error));
-      } else {
-        seriesCallback();
-      }
-    },
-    (seriesCallback) => {
-      updateTranslation(item, seriesCallback);
-    },
+    (parallelCallback) => {
+      cloneTranslationForItem("postContent", inPost.id, outPost.id, inPost.description, parallelCallback);
+    }
   ], error => {
     done(error);
   })
-};
+}
+
+const cloneTranslationForCommunity = (inCommunity, outCommunity, done) => {
+  async.parallel([
+    (parallelCallback) => {
+      cloneTranslationForItem("communityName", inCommunity.id, outCommunity.id, inCommunity.name, parallelCallback);
+    },
+    (parallelCallback) => {
+      cloneTranslationForItem("communityContent", inCommunity.id, outCommunity.id, inCommunity.description, parallelCallback);
+    }
+  ], error => {
+    done(error);
+  })
+}
+
+const cloneTranslationForGroup = (inGroup, outGroup, done) => {
+  async.parallel([
+    (parallelCallback) => {
+      cloneTranslationForItem("groupName", inGroup.id, outGroup.id, inGroup.name, parallelCallback);
+    },
+    (parallelCallback) => {
+      cloneTranslationForItem("groupContent", inGroup.id, outGroup.id, inGroup.objectives, parallelCallback);
+    }
+  ], error => {
+    done(error);
+  })
+}
+
+const cloneTranslationForPoint = (inPoint, outPoint, done) => {
+  async.parallel([
+    (parallelCallback) => {
+      cloneTranslationForItem("pointContent", inPoint.id, outPoint.id, inPoint.PointRevisions[inPoint.PointRevisions.length-1].content, parallelCallback);
+    }
+    ], error => {
+    done(error);
+  })
+}
+
 
 module.exports = {
-  getTranslatedTextsForCommunity,
-  getTranslatedTextsForGroup,
-  updateTranslationForCommunity,
-  updateTranslationForGroup
+  cloneTranslationForPoint,
+  cloneTranslationForPost,
+  cloneTranslationForCommunity,
+  cloneTranslationForGroup
 };
