e7489b30d0055909d8632d7fa01184ad2ad664a3
Robert Bjarnason <robert.bjarnason@gmail.com>
2021-08-20 23:19:15 +0000

    Work on reports

--- workers/reports.js
+++ workers/reports.js
@@ -8,6 +8,7 @@ const _ = require('lodash');
 const fs = require('fs');
 const createDocxReport = require('../engine/reports/docx_group_report').createDocxReport;
 const createXlsReport = require('../engine/reports/xls_group_report').createXlsReport;
+const createXlsCommunityUsersReport = require('../engine/reports/xls_community_users_report').createXlsCommunityUsersReport;
 
 let airbrake = null;
 if(process.env.AIRBRAKE_PROJECT_ID) {
@@ -23,6 +24,9 @@ ReportsWorker.prototype.process = (workPackage, callback) => {
       break;
     case 'start-xls-report-generation':
       createXlsReport(workPackage, callback);
+      break;
+    case 'start-xls-users-community-report-generation':
+      createXlsCommunityUsersReport(workPackage, callback);
       break;
     default:
       callback("Unknown type for workPackage: " + workPackage.type);
