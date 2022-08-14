6f63a841f1d862ab08b9367c82209a6d4244aa1d
Robert Bjarnason <robert.bjarnason@gmail.com>
2022-08-14 03:01:10 +0000

    Work

--- engine/analytics/plausible/manager.js
+++ engine/analytics/plausible/manager.js
@@ -182,12 +182,21 @@ async function plausibleStatsProxy(plausibleUrl) {
     if (process.env["PLAUSIBLE_BASE_URL"] && process.env["PLAUSIBLE_API_KEY"]) {
       const baseUrl = process.env["PLAUSIBLE_BASE_URL"].replace("/api/v1/", "");
       const options = {
-        url: baseUrl + plausibleUrl + `&site_id=${process.env.PLAUSIBLE_SITE_NAME}`,
+        //url: baseUrl+ plausibleUrl, //+ `&site_id=${process.env.PLAUSIBLE_SITE_NAME}`,
+        url: "https://pl-eu.citizens.is" + "/api/stats/localhost/top-stats?period=30d&date=2022-08-14&filters=%7B%7D&with_imported=true",
         headers: {
+          //Authorization: `Bearer ${process.env["PLAUSIBLE_API_KEY"]}`,
+          //Authorization: `Basic cGwtZXUtNDNmLXNkZDl0ajM6ZGM4Y1g5dHYyMzQubnM3dmp3cExGcGU4Z25Da2Q5Z2pTZDBmbHNh`,
+          //"X-Forwarded-For": "194.144.7.167,127.0.0.1",
+          //"Content-Type": "application/json",
+          //"UserAgent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.3",
+          "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.3",
+          //referrer: "https://pl-eu.citizens.is/localhost",
+          //referer: "https://pl-eu.citizens.is/localhost",
           Authorization: `Bearer ${process.env["PLAUSIBLE_API_KEY"]}`,
-          "X-Forwarded-For": "127.0.0.1",
+          "X-Forwarded-For": "194.144.7.167,127.0.0.1",
           "Content-Type": "application/json",
-        },
+        }
       };
 
       log.info(JSON.stringify(options));
