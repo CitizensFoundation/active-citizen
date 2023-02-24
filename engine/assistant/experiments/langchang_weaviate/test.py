c1137b0f3ac08bdabf754d6c04dbaab3eaadc89a
Robert Bjarnason <robert.bjarnason@gmail.com>
2023-02-24 20:13:40 +0000

    Work on AI assistant

--- engine/assistant/experiments/langchang_weaviate/test.py
+++ engine/assistant/experiments/langchang_weaviate/test.py
@@ -20,7 +20,7 @@ print(json.dumps(result, indent=4))
 
 vectorstore = Weaviate(client, "Posts", "englishNameAndContent")
 
-MyOpenAI = OpenAI(temperature=0.2,
+MyOpenAI = OpenAI(temperature=0.7,
     openai_api_key=os.environ.get("OPENAI_API_KEY"))
 
 qa = ChatVectorDBChain.from_llm(MyOpenAI, vectorstore, verbose=True)
@@ -31,6 +31,7 @@ print("Welcome to the My Neighborhood AI Assistant!")
 print("Ask me anything about our project.")
 
 while True:
+    print("")
     query = input("")
     result = qa({"question": query, "chat_history": chat_history})
     print(result["answer"])
