4b47125edd150af983d2c518a363a6517fb33741
Robert Bjarnason <robert.bjarnason@gmail.com>
2023-03-04 16:50:33 +0000

    Work on AI assistant

---
+++ engine/assistant/ai-assistant-client/testOnePost.js
@@ -0,0 +1,21 @@
+const models = require('../../../models');
+const _ = require('lodash');
+const async = require('async');
+const log = require('../../../utils/logger');
+const updateCollection = require('./aiAssistantClient').updateCollection;
+
+if (process.env["AC_AI_ASSISTANT_KEY"] && process.env["AC__AI_ASSISTANT_URL"] ) {
+  updateCollection({
+    postId: 71592
+  }, (error, result) => {
+    if (error) {
+      console.error(error);
+    } else {
+      console.log(result);
+    }
+    process.exit();
+  });
+} else {
+  console.error("NO AC_AI_ASSISTANT_KEY");
+  process.exit();
+}
