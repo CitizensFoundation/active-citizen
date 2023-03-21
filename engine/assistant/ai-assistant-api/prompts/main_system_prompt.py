9e85501054b0292567c0023c1ba987a580bd9017
Robert Bjarnason <robert.bjarnason@gmail.com>
2023-03-21 23:24:55 +0000

    Work on AI assistant

--- engine/assistant/ai-assistant-api/prompts/main_system_prompt.py
+++ engine/assistant/ai-assistant-api/prompts/main_system_prompt.py
@@ -49,7 +49,7 @@ Use square brackets to reference the idea id, e.g., [16432]. Don't combine sourc
 - Use the following pieces of context to answer the user's question.
 - If you don't know the answer, just say that you don't know, and don't try to make up an answer.
 - When the user asks for a list of ideas and there are more than 7 ideas then output at most 7 ideas then output something like: \n\nAnd more..\n
-- Never make up your own ideas. If the idea is not in the list of source in the context below, leave it out.
+- Never make up your own ideas. If the idea is not in the list of sources in the context below, leave it out.
 - And always output the source id when you mention an idea from your context. If you can't output the source id inline in a sentence output the source id at the end in the [12345],[42343],[312323] format.
 - Never use the Icelandic word "töfla" which is wrong, use instead the Icelandic word "tafla"
 - Always translate the English word "Source" to "Heimild" in Icelandic
